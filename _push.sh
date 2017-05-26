be6bdb5b8fd48f291452a80dc50e03bef8907efa
Ben Best <bdbest@gmail.com>
2017-05-26 12:51:13 -0700

    +rmarkdown, build->render deploy->push

---
+++ _push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+export GH_USER=sustainsb
+export GH_REPO=sustainsb.github.io
+export GH_BRANCH=master
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b ${GH_BRANCH}
+  git add . *.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin ${GH_BRANCH}
+}
+
+setup_git
+commit_website_files
+upload_files
