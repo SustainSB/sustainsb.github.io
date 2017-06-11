ac9c1574bdeb83ea99f4b12a46d011078bf81c1b
Ben Best <ben@ecoquants.com>
2017-06-11 07:06:56 -0700

    simplified _push.sh with git push ... HEAD:GH_BRANCH

--- _push.sh
+++ _push.sh
@@ -4,22 +4,14 @@ export GH_USER=SustainSB
 export GH_REPO=sustainsb.github.io
 export GH_BRANCH=master
 
-setup_git() {
-  git config --global user.email "ben@ecoquants.com"
-  git config --global user.name "Ben Best"
-  git config --global push.default matching
-}
+# setup git
+git config --global user.email "ben@ecoquants.com"
+git config --global user.name "Ben Best"
+git config --global push.default matching
 
-commit_website_files() {
-  #git checkout ${GH_BRANCH}
-  git add --all
-  git commit -am "Travis render & push [skip ci]: $TRAVIS_BUILD_NUMBER"
-}
+# git commit
+git add --all
+git commit -am "Travis render & push [skip ci]: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git push https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git ${GH_BRANCH}
-}
-
-setup_git
-commit_website_files
-upload_files
+# git push
+git push https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git HEAD:${GH_BRANCH}
